7bdbf3d47f436ff1e316f21e3905c2825044090d
Ryan Hodges <ryan.d.hodges@gmail.com>
2017-02-02 15:54:55 -0800

    creating marineplanner project with core app - skip this template bs

--- marineplanner/marineplanner/wsgi.py
+++ marineplanner/marineplanner/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for template project.
+WSGI config for marineplanner project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "template.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "marineplanner.settings")
 
 application = get_wsgi_application()
