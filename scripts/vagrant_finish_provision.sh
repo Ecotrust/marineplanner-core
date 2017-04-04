34154020e625ab07501fa1980bcdcf82eca6d5a3
Ryan Hodges <ryan.d.hodges@gmail.com>
2017-04-04 14:50:13 -0700

    fixing order of installing dependencies before migrating, etc...

--- scripts/vagrant_finish_provision.sh
+++ scripts/vagrant_finish_provision.sh
@@ -7,15 +7,25 @@ VIRTUALENV_DIR=$PROJECT_DIR/env
 
 PYTHON=$VIRTUALENV_DIR/bin/python
 
+echo "Beginning finishing stages of provisioning"
+
 ### HACKY HACKS!!! (for mp-visualize, mostly)
+echo "Linking static content into static directory"
 if [ ! -e $PROJECT_DIR/$APP_NAME/static/bower_components ]; then
-  `ln -s $PROJECT_DIR/bower_components/ $PROJECT_DIR/$APP_NAME/static/bower_components`
+  ln -s $PROJECT_DIR/bower_components/ $PROJECT_DIR/$APP_NAME/static/bower_components
 fi
 
 if [ ! -e $PROJECT_DIR/$APP_NAME/static/bundles ]; then
-  `ln -s $PROJECT_DIR/assets/javascript/ $PROJECT_DIR/$APP_NAME/static/bundles`
+  ln -s $PROJECT_DIR/assets/javascript/ $PROJECT_DIR/$APP_NAME/static/bundles
 fi
+### END HACKY HACKS
 
-`$PYTHON $PROJECT_DIR/$APP_NAME/manage.py migrate`
-`$PYTHON $PROJECT_DIR/$APP_NAME/manage.py collectstatic`
-`$PYTHON $PROJECT_DIR/$APP_NAME/manage.py compress`
+echo "Migrating the DB"
+$PYTHON $PROJECT_DIR/$APP_NAME/manage.py migrate --noinput
+
+echo "Collecting Static Files"
+yes yes | $PYTHON $PROJECT_DIR/$APP_NAME/manage.py collectstatic
+rm -rf $PROJECT_DIR/static/modules
+
+echo "Compressing Files"
+$PYTHON $PROJECT_DIR/$APP_NAME/manage.py compress
