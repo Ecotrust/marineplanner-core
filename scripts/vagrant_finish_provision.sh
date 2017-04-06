a2597a0a20ff49748f549a03e3e905920fda52d3
Ryan Hodges <ryan.d.hodges@gmail.com>
2017-04-05 17:38:04 -0700

    you can't link to static until static exists

--- scripts/vagrant_finish_provision.sh
+++ scripts/vagrant_finish_provision.sh
@@ -9,6 +9,13 @@ PYTHON=$VIRTUALENV_DIR/bin/python
 
 echo "Beginning finishing stages of provisioning"
 
+echo "Migrating the DB"
+$PYTHON $PROJECT_DIR/$APP_NAME/manage.py migrate --noinput
+
+echo "Collecting Static Files"
+yes yes | $PYTHON $PROJECT_DIR/$APP_NAME/manage.py collectstatic
+rm -rf $PROJECT_DIR/static/modules
+
 ### HACKY HACKS!!! (for mp-visualize, mostly)
 echo "Linking static content into static directory"
 if [ ! -e $PROJECT_DIR/$APP_NAME/static/bower_components ]; then
@@ -19,13 +26,6 @@ if [ ! -e $PROJECT_DIR/$APP_NAME/static/bundles ]; then
   ln -s $PROJECT_DIR/assets/javascript/ $PROJECT_DIR/$APP_NAME/static/bundles
 fi
 ### END HACKY HACKS
-
-echo "Migrating the DB"
-$PYTHON $PROJECT_DIR/$APP_NAME/manage.py migrate --noinput
-
-echo "Collecting Static Files"
-yes yes | $PYTHON $PROJECT_DIR/$APP_NAME/manage.py collectstatic
-rm -rf $PROJECT_DIR/static/modules
 
 echo "Compressing Files"
 $PYTHON $PROJECT_DIR/$APP_NAME/manage.py compress
