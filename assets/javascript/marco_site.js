4c6a1b4083348a4bb346c6aa37d3b76f876bae00
Ryan Hodges <ryan.d.hodges@gmail.com>
2017-06-28 15:22:07 -0700

    small touch-ups

--- assets/javascript/marco_site.js
+++ assets/javascript/marco_site.js
@@ -1,7 +1,7 @@
-var ga = require('./ga');
-require('./navbar')($('.navbar-default'));
+var ga = require(['./ga']);
+require(['./navbar'])($('.navbar-default'));
 
 // for data catalog and MP
-require('bower_components/bootstrap/js/tooltip');
+require(['bower_components/bootstrap/js/tooltip']);
 // for MP
-require('bower_components/bootstrap/js/popover');
+require(['bower_components/bootstrap/js/popover']);
